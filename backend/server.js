d5a3af30b325112abf823a429f668d3b8cb20e0a
vasim-qureshi <vasimqureshi2000@gmail.com>
2025-05-03 00:54:12 +0530

    add client_url in .env backend

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: process.env.Client_URL, credentials: true }));
 
 // Connect Database
 connectDB();
